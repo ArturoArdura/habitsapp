95181bf1a249d8b6a837f2faec29986323997b1e
arturo <arturo@kai.com>
2025-11-06 20:01:00 -0600

    First commit to get millonaire

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+
+import { Text, View } from 'react-native';
+import "../global.css";
+
+
+
+export default function Index() {
+  return (
+    <View >
+      <Text>This is a modal</Text>
+      <Text>Go to home screen</Text>
+    </View>
+  );
+}
+
